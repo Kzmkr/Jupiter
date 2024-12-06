ee8f7df449ce8ca006530f71ffe8555c3f613422
Artghost690204 <horvathpal16@gmail.com>
2024-12-06 23:09:47 +0100

    Teacher fájlok megvannak, működnek teljesen

--- src/main/java/org/example/jupiter/api/controller/UserController.java
+++ src/main/java/org/example/jupiter/api/controller/UserController.java
@@ -1,8 +1,10 @@
 package org.example.jupiter.api.controller;
 
 import org.example.jupiter.api.model.Student;
+import org.example.jupiter.api.model.Teacher;
 import org.example.jupiter.api.model.User;
 import org.example.jupiter.service.StudentService;
+import org.example.jupiter.service.TeacherService;
 import org.example.jupiter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,12 +22,14 @@ public class UserController {
 
     private final UserService userService;
     private final StudentService studentService;
+    private final TeacherService teacherService;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserService userService,StudentService studentService, PasswordEncoder passwordEncoder){
+    public UserController(UserService userService,StudentService studentService,TeacherService teacherService, PasswordEncoder passwordEncoder){
         this.userService = userService;
         this.studentService = studentService;
+        this.teacherService = teacherService;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -47,6 +51,12 @@ public class UserController {
     public ResponseEntity<String> addUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password, @RequestParam String role) {
         if ("STUDENT".equalsIgnoreCase(role)) {
             studentService.addStudent(new Student(firstName, lastName, email, passwordEncoder.encode(password), role));
+        } else if ("USER".equalsIgnoreCase(role)) {
+            User user = new User( firstName, lastName, email, passwordEncoder.encode(password), role);
+            userService.addUser(user);
+        }
+        if ("TEACHER".equalsIgnoreCase(role)) {
+            teacherService.addTeacher(new Teacher(firstName, lastName, email, passwordEncoder.encode(password), role));
         } else if ("USER".equalsIgnoreCase(role)) {
             User user = new User( firstName, lastName, email, passwordEncoder.encode(password), role);
             userService.addUser(user);
