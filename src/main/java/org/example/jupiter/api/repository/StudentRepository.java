036b7094f56051447b5e72090adb581ba5907f15
Kzmkr <KOZMA.KRISTOF14@GMAIL.COM>
2024-12-07 17:58:41 +0100

    Student api test

--- src/main/java/org/example/jupiter/api/repository/StudentRepository.java
+++ src/main/java/org/example/jupiter/api/repository/StudentRepository.java
@@ -3,7 +3,9 @@ package org.example.jupiter.api.repository;
 import org.example.jupiter.api.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, String> { }
+public interface StudentRepository extends JpaRepository<Student, String> {
+    Student findByEmail(String email);
+}
 
 
 
